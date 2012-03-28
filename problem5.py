28223070b25676ccbe6c6b20459a7ddc312a6642
Vaibhav Singh <techvaibhav@gmail.com>
2012-03-28 13:46:17 +0100

    Incrementing by 2520 to improve performance

--- problem5.py
+++ problem5.py
@@ -1,12 +1,16 @@
 def is_divisible(number):
-    for i in range (1, 20):
+    for i in range(11, 21):
         if number%i != 0:
             return False
     return True
 
 if __name__ == "__main__":
     import itertools
-    for i in itertools.count(1):
+    # Since 2520 is divisible by all numbers up to 10.
+    # The new number has to be divisible by 2520
+    i = 2520
+    while True:
         if is_divisible(i):
             print i
             break
+        i += 2520
