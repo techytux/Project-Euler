0ad6b302b49b628b77c8a7013d76038fe05f3c07
Vaibhav Singh <techvaibhav@gmail.com>
2012-04-04 21:02:17 +0100

    added documentation

--- problem9.py
+++ problem9.py
@@ -1,4 +1,7 @@
 def problem9():
+    '''There exists exactly one Pythagorean triplet for which a + b + c = 1000.
+    Find the product abc.'''
+
     for a in range(1, 500):
         for b in range(1, 500):
             c = 1000 - a - b
