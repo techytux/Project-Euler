b280a9aaa73ae95592f57b78979859a4b7b94ebb
Vaibhav Singh <techvaibhav@gmail.com>
2012-04-04 21:01:29 +0100

    added documentation, getting rid of sorting in reverse order for the list instead using -1 as the index

--- problem3.py
+++ problem3.py
@@ -1,4 +1,7 @@
 def problem3(n):
+    '''The prime factors of 13195 are 5, 7, 13 and 29.
+    
+    What is the largest prime factor of the number 600851475143 ?'''
     factors = []
     div = 2
     if n%div==0:
@@ -9,8 +12,7 @@ def problem3(n):
             factors.append(div)
             n /= div
         div += 2
-    factors.sort(reverse=True)
-    return factors[0]
+    return factors[-1]
 
 if __name__ == "__main__":
     import sys
